13e64587caec6fa10d72540d136ed1e5b10af99b
Katia Bulekova <ktrn@bu.edu>
2025-01-13 11:42:14 -0500

    Output overall statistics about jobs (Josh slide #4)

--- graphs.py
+++ graphs.py
@@ -24,17 +24,40 @@ time_period = "January 1 - November 25, 2024"
 print(df.columns)
 print(df.head())
 print(df.tail())
-
-
 freq_table = df["job_name"].value_counts()
 print(freq_table.head(10))
+
+#%%
+# Get values from Josh's slide # 4
+# 192,000 jobs, 1.4 million GPU hours
+# 1051 users, 380 projects
+# 81 hosts, 47 queues
+
+# Total number of jobs
+print(" Total number of jobs", f'{len(df):,}' )
+
+# Number of unique users
+print(" Total number of unique users", f'{df['owner'].nunique():,}' )
+
+# Number of unique projects
+print(" Number of unique projects", df['project'].nunique())
+
+# number of unique GPU nodes used
+print(" Number of GPU nodes", df['hostname'].nunique())
+
+# Number of unique queues (probably not useful)
+print(" Number of unique GPU queues", df['qname'].nunique())
+
+
+
+
 
 
 #%%
 job_class = job_classification(df["job_name"])
 counts = job_class.value_counts()
-print("Counts of True and False values:")
-print(counts)
+# print("Counts of True and False values:")
+# print(counts)
 
 # Split the original dataframe into two
 df_interactive = df[job_class]  # DataFrame where the condition is True
@@ -46,13 +69,12 @@ plot_job_classification(counts,
             title="Batch vs. Interactive (OOD or qrsh) job",
             subtitle = time_period )
 
-
+#%%
 # Compute the total wallclock time for the batch jobs vs. interactive
 int_WC_time = round(df_interactive['ru_wallclock'].sum()/3600)
 batch_WC_time = round(df_batch['ru_wallclock'].sum()/3600)
 
 # Graph2: Display Interactive vs batch GPU jobs by wallclock time
-
 jobs_WC = pd.Series([batch_WC_time,int_WC_time])
 plot_job_time_classification(jobs_WC, 
             title="Batch vs. Interactive (OOD or qrsh) job by time",
